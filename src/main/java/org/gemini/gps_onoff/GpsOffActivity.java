ecded31f37f27fc08ccc684547274ecfe12bd262
Zijie He <zijiehe@chromium.org>
2016-08-26 17:12:30 -0700

    Initial commit

---
+++ src/main/java/org/gemini/gps_onoff/GpsOffActivity.java
@@ -0,0 +1,31 @@
+package org.gemini.gps_onoff;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Process;
+import android.provider.Settings;
+
+public final class GpsOffActivity extends Activity
+{
+    @Override
+    @SuppressWarnings("deprecation")
+    protected void onCreate(Bundle bundle)
+    {
+        super.onCreate(bundle);
+        Settings.Secure.setLocationProviderEnabled(
+            getContentResolver(), LocationManager.GPS_PROVIDER, false);
+        Settings.Secure.setLocationProviderEnabled(
+            getContentResolver(), LocationManager.NETWORK_PROVIDER, false);
+        finish();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Process.killProcess(Process.myPid());
+        System.exit(0);
+    }
+}
